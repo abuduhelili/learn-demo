3eff0605c8b432706c2045ca82792170bec6b251
Harry <2062454635@qq.com>
2024-06-08 23:45:45 +0800

    --

--- ts-demo/src/main.ts
+++ ts-demo/src/main.ts
@@ -1,20 +1,6 @@
 {
-  // 泛型：类型约束
-
-  // 定义接口
-  interface ILength {
-    length: number;
+  // 泛型
+  function identity<T>(arg: T): T {
+    return arg;
   }
-  // 添加约束(给泛型添加更高的约束)
-  function get<T extends ILength>(val: T) {
-    console.log(val.length);
-    return val;
-  }
-
-  //
-  // 转换成数组：
-  // function get<T>(val: T[]) {
-  //   return val;
-  // }
-  // let res = get([1, 2, 3, 'aaa']);
 }
